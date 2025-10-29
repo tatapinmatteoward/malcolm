398827c1cc3afe93da748ceb071d0b8b3d8bfaff
tatapinmatteoward <athcdejd81rul5@hotmail.com>
2025-10-29 10:30:26 +0000

    Add .vitepress/theme/posts.data.js

---
+++ .vitepress/theme/posts.data.js
@@ -0,0 +1,45 @@
+export default {
+  load() {
+    return [
+    {
+        "url": "/blog/how-to-fix-bsod-windows-10",
+        "title": "How to Fix Blue Screen of Death (BSOD) on Windows 10: Ultimate Guide",
+        "excerpt": "Tired of BSOD on Windows 10? Learn effective fixes for common Blue Screen of Death errors, from updating drivers to system recovery.",
+        "date": "2023-10-15",
+        "tags": [
+            "Windows 10",
+            "BSOD",
+            "Troubleshooting",
+            "PC Fixes",
+            "System Errors"
+        ]
+    },
+    {
+        "url": "/blog/quick-fixes-macos-app-crashes-freezes",
+        "title": "Quick Fixes for macOS App Crashes and Freezes: Boost Your Mac's Performance",
+        "excerpt": "Struggling with macOS apps crashing or freezing? Discover simple, effective solutions to troubleshoot and prevent these issues, keeping your Mac running smoothly.",
+        "date": "2023-10-05",
+        "tags": [
+            "macOS",
+            "troubleshooting",
+            "app crashes",
+            "mac tips",
+            "software fixes"
+        ]
+    },
+    {
+        "url": "/blog/fix-startup-issues-windows-macos",
+        "title": "Fix Startup Issues on Windows and macOS: Ultimate Troubleshooting Guide",
+        "excerpt": "Struggling with slow boot times or failed startups on Windows or macOS? Discover expert tips to diagnose and resolve common startup problems quickly and efficiently.",
+        "date": "2023-10-05",
+        "tags": [
+            "Windows",
+            "macOS",
+            "troubleshooting",
+            "startup issues",
+            "tech fixes"
+        ]
+    }
+]
+  }
+}
